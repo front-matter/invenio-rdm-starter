5cc5a28c1e104dd8b44708f9e5499ff81c2ceaa0
Martin Fenner <martin@front-matter.io>
2024-06-25 10:09:56 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,48 +1,39 @@
 #!/bin/bash
 
-SETUP_COMPLETED="SETUP_COMPLETED"
-if [ ! -e $SETUP_COMPLETED ]; then
-    echo "-- Setup InvenioRDM --"
+echo "-- Setup InvenioRDM --"
 
-    # Creating database...
-    invenio db init create
+# Creating database...
+invenio db init create
 
-    # Creating files location...
-    invenio files location create --default default-location /opt/invenio/var/instance/data
+# Creating files location...
+invenio files location create --default default-location /opt/invenio/var/instance/data
 
-    # Creating admin role...
-    invenio roles create admin
+# Creating admin role...
+invenio roles create admin
 
-    # Assigning superuser access to admin role...
-    invenio access allow superuser-access role admin
+# Assigning superuser access to admin role...
+invenio access allow superuser-access role admin
 
-    # Dropping and re-reating indices...
-    invenio index destroy --force --yes-i-know
-    invenio index init
+# Dropping and re-reating indices...
+invenio index destroy --force --yes-i-know
+invenio index init
 
-    # Creating custom fields for records...
-    invenio rdm-records custom-fields init
+# Creating custom fields for records...
+invenio rdm-records custom-fields init
 
-    # Creating custom fields for communities...
-    invenio communities custom-fields init
+# Creating custom fields for communities...
+invenio communities custom-fields init
 
-    # Creating rdm fixtures if...
-    invenio rdm-records fixtures
+# Creating rdm fixtures if...
+invenio rdm-records fixtures
 
-    # Creating demo records...
-    invenio rdm-records demo records --user user@demo.org
+# Creating demo records...
+invenio rdm-records demo records --user user@demo.org
 
-    # Creating demo communities
-    invenio rdm-records demo communities --user community@demo.org
+# Creating demo communities
+invenio rdm-records demo communities --user community@demo.org
 
-    # Declaring queues...
-    invenio queues declare
+# Declaring queues...
+invenio queues declare
 
-    touch $SETUP_COMPLETED
-    echo "-- Setup completed --"
-else
-    echo "-- No setup needed --"
-fi
-
-# Start application server
-gunicorn invenio_app.wsgi:application --bind 0.0.0.0:5000 --workers=4
+echo "-- Setup completed --"
